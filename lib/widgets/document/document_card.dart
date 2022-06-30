351394e3aa660359bd2e6b28b0e80e13fa2157df
Joshua Kidd <hey@g33kidd.dev>
2022-06-30 08:31:24 -0600

    card updates

--- lib/widgets/document/document_card.dart
+++ lib/widgets/document/document_card.dart
@@ -24,68 +24,65 @@ class DocumentCard extends StatelessWidget {
           },
           child: Padding(
             padding: const EdgeInsets.fromLTRB(12, 12, 12, 6),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                DocumentPath(
-                  path: resourceItem.documentPath ?? "N/A",
-                ),
-                const SizedBox(
-                  height: 8.0,
-                ),
-                DocumentTitle(
-                  title: resourceItem.resourceTitle ?? "N/A",
-                ),
-                const SizedBox(
-                  height: 10.0,
-                ),
-                Row(
-                  children: <Widget>[
-                    DocumentPubDate(
-                      date: resourceItem.resourceRevisionDate ?? "N/A",
-                    ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    const DocumentAuthor(
-                      author: "USDA",
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 6.0,
-                ),
-                DocumentKeywords(
-                  keywords: resourceItem.keywords ?? "N/A",
-                ),
-                const SizedBox(
-                  height: 6.0,
-                ),
-                Row(
-                  children: <Widget>[
-                    DocumentType(
-                        type: resourceItem.resourceMimeType?.mimeTypeName ??
-                            "--"),
-                    Spacer(),
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.bookmark_add_outlined,
-                        color: NrcsBlue,
-                        size: 20,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      DocumentPath(
+                        path: resourceItem.documentPath ?? "N/A",
                       ),
-                      visualDensity: VisualDensity.compact,
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.download_outlined,
-                        color: NrcsBlue,
-                        size: 20,
+                      const SizedBox(height: 8.0),
+                      DocumentTitle(
+                        title: resourceItem.resourceTitle ?? "N/A",
                       ),
-                      visualDensity: VisualDensity.compact,
-                    )
-                  ],
+                      const SizedBox(height: 10.0),
+                      Row(
+                        children: <Widget>[
+                          DocumentPubDate(
+                            date: resourceItem.resourceRevisionDate ?? "N/A",
+                          ),
+                          const SizedBox(width: 10),
+                          const DocumentAuthor(
+                            author: "USDA",
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 6.0),
+                      DocumentKeywords(
+                        keywords: resourceItem.keywords ?? "N/A",
+                      ),
+                      const SizedBox(height: 6.0),
+                      Row(
+                        children: <Widget>[
+                          DocumentType(
+                              type:
+                                  resourceItem.resourceMimeType?.mimeTypeName ??
+                                      "--"),
+                          Spacer(),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.bookmark_add_outlined,
+                              color: NrcsBlue,
+                              size: 20,
+                            ),
+                            visualDensity: VisualDensity.compact,
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.download_outlined,
+                              color: NrcsBlue,
+                              size: 20,
+                            ),
+                            visualDensity: VisualDensity.compact,
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
